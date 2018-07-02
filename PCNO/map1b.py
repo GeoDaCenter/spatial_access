bfb1c8ce599e26af4a06debc887fbedf0aa90851
Erin M. Ochoa <emo@uchicago.edu>
2018-07-02 14:07:18 -0500

    Outputs the dataframe for map 1b

---
+++ PCNO/map1b.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+
+
+HQ = '../../../PCNO/CSV/chicago/contracts_w_hq_addresses.csv'
+GEO = '../../../PCNO/CSV/chicago/Geocoded Service Addresses/map1_addresses_geocoded.csv'
+CATEGORIES = '../../../PCNO/Rule-based classification/chi_cook_il_classified.csv'
+MAP1B = '../../../PCNO/CSV/chicago/Maps/map1b.csv'
+
+
+def read_geo():
+    '''
+    Reads in the geocoded addresses for map 1. Drops the 'Match Score' column.
+    Returns a dataframe.
+    '''
+
+    df = pd.read_csv(GEO)
+    df = df.drop('Match Score',axis=1)
+
+    return df
+
+
+def read_contracts():
+    '''
+    Reads in the contracts with headquarter addresses. Returns a dataframe.
+    '''
+
+    df = pd.read_csv(HQ)
+
+    return df
+
+
+def read_categories():
+    '''
+    Reads in the service-code classifications for the contracts. Returns a
+    dataframe.
+    '''
+
+    df = pd.read_csv(CATEGORIES)
+    df = df[['Classification','Contract Number']]
+    df = df.rename(columns={'Contract Number':'ContractNumber'},index=str)
+
+    return df
+
+
+def merge():
+    '''
+    Reads in the geocoded headquarter addresses, the contracts, and the service
+    classifications. Merges the dataframes together. Returns a dataframe.
+    '''
+
+    geo = read_geo()
+
+    contracts = read_contracts()
+
+    cats = read_categories()
+
+    df = contracts.merge(geo)
+    df = df.merge(cats)
+
+    return df
+
+
+if __name__ == '__main__':
+
+    merged = merge()
+    merged.to_csv(MAP1B,index=False)
