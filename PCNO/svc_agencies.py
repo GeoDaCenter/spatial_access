c5ee052f30006dccf3258138e8f3e412ce7f9a87
Erin M. Ochoa <emo@uchicago.edu>
2018-07-06 12:49:33 -0500

    Added the dollars_per_location function; working on needs_geocoding

--- PCNO/svc_agencies.py
+++ PCNO/svc_agencies.py
@@ -1,6 +1,7 @@
 import UTILS as u
 import numpy as np
 import pandas as pd
+import COMPARE_ADDRESSES as ca
 
 
 DEDUPED = '../../../rcc-uchicago/PCNO/CSV/chicago/2018-07-03_link_agencies_output.csv'
@@ -37,24 +38,52 @@ def merger():
     return merged
 
 
+def dollars_per_location():
+    '''
+    '''
+
+    link = linker()
+    svc = read_svc()
+    hq = read_hq()[['CSDS_Vendor_ID','VendorName','Agency_Summed_Amount']].drop_duplicates()
+
+    merged = hq.merge(link,how='left').merge(svc,how='inner')
+
+    merged = merged.assign(Dollars_Per_Location=merged['Agency_Summed_Amount']\
+                           / merged['Num_Svc_Locations'])
+
+    return merged.reset_index(drop=True)
+
+
 def read_hq():
     '''
     '''
 
     df = pd.read_csv(HQ,converters={'Zip':str})
+    agg_amounts = agg_funds(df)
+    merged = df.merge(agg_amounts)
 
-    return df
+    return merged
 
 
 def read_svc():
     '''
     '''
 
+    print('\nReading in service agencies')
+
     df = pd.read_csv(SVC,converters={'ZipCode':str})
     df = u.rename_cols(df,[x for x in df.columns if x != 'CSDS_Svc_ID'],'_SVC')
     df = df.rename(columns={'CSDS_Svc_ID':'CSDS_Vendor_ID_LINK2'},index=str)
 
-    return df
+    key = 'CSDS_Vendor_ID_LINK2'
+    target = 'Address_SVC'
+    fixed_addresses = ca.fix_duplicate_addresses(df,key,target)
+    fixed_addresses = fixed_addresses.drop_duplicates(subset=[key,target])
+
+    counts = count_svc_addr(fixed_addresses)
+    merged = fixed_addresses.merge(counts)
+
+    return merged.reset_index(drop=True)
 
 
 def read_deduplicated():
@@ -75,6 +104,7 @@ def agg_funds(hq):
     '''
 
     agg = hq.groupby('CSDS_Vendor_ID')['Amount'].sum().reset_index()
+    agg = agg.rename(columns={'Amount':'Agency_Summed_Amount'},index=str)
 
     return agg
 
@@ -83,17 +113,44 @@ def count_svc_addr(df):
     '''
     '''
 
-    counts = df.groupby('ClusterID')['CSDS_Vendor_ID_LINK2'].value_counts()
-    counts.name = 'WhatThisCounts'
-    return counts
+    #counts = df.groupby('ClusterID')['CSDS_Vendor_ID_LINK2'].value_counts()
+    df = df.assign(Constant=1)
+    counts = df.groupby('CSDS_Vendor_ID_LINK2')['Constant'].count()
+    counts.name = 'Num_Svc_Locations'
+
+    return counts.to_frame().reset_index()
 
 
-    return df
+def needs_geocoding(df,id,lat,lon,address_fields):
+    '''
+    '''
+
+    #address,city,state,zipcode = address_fields
+
+    needs_geo = df[[id,lat,lon, x for x in address_fields]].drop_duplicates()
+    needs_geo = needs_geo[pd.isnull(needs_geo[lat,lon])]
+    needs_geo = needs_geo.drop([lat,lon],axis=1)
+
+    new_cols = [re.sub('_SVC$','',x) for x in needs_geo.columns]
+    cn = dict(zip(needs_geo.columns,new_cols))
+
+    needs_geo = needs_geo.rename(columns=cn,index=str)
+    needs_geo = needs_geo.rename(columns={'ZipCode':'Zip'},index=str)
+
+    return needs_geo.reset_index(drop=True)
+
 
 
 if __name__ == '__main__':
 
-    merged = merger()
+    print()
+    #merged = merger()
+    dollars_div = dollars_per_location()
+    dollars_div.to_csv('../../../rcc-uchicago/PCNO/CSV/chicago/dollars_div.csv',index=False)
+
+    lat = 'Latitude_SVC'
+    lon = 'Longitude_SVC'
+    id_col =
 
 
 
@@ -103,9 +160,23 @@ if __name__ == '__main__':
     '''
     $/agency @ service addresses
 
-
-
     CONTRACTAGENCIES [VendorName] LINK [Name] SERVICEAGENCIES
+
+    -Count the number of service addresses per agency; add as a column
+    -Add agg_dollars as a column
+    -Divide agg_dollars by num_locations
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
