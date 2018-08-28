a6a099fc9644a6675bec4f5d40b345191f859f25
Erin M. Ochoa <emo@uchicago.edu>
2018-08-27 21:11:52 -0500

    Fixed some bugs (bad merge for coordinates, additional geocoded files, etc.)

--- PCNO/map2.py
+++ PCNO/map2.py
@@ -5,7 +5,9 @@ import pandas as pd
 
 
 DOLLARS_DIVIDED = '../../../rcc-uchicago/PCNO/CSV/chicago/dollars_divided.csv'
-GEO = '../../../rcc-uchicago/PCNO/CSV/chicago/Geocoded Service Addresses/map2_geocoding_geocoded.csv'
+GEO1 = '../../../rcc-uchicago/PCNO/CSV/chicago/Geocoded Service Addresses/map2_geocoding_geocoded.csv'
+GEO2 = '../../../rcc-uchicago/PCNO/CSV/chicago/Geocoded Service Addresses/map2_geocoding_version2_geocoded.csv'
+GEO3 = '../../../rcc-uchicago/PCNO/CSV/chicago/Geocoded Service Addresses/map2_geocoding_version3_geocoded.csv'
 MAP2_SATELLITES = '../../../rcc-uchicago/PCNO/CSV/chicago/Maps/map2_satellites.csv'
 MAP2_HQ = '../../../rcc-uchicago/PCNO/CSV/chicago/Maps/map2_hq.csv'
 
@@ -16,19 +18,32 @@ def read_geo():
     Returns a dataframe.
     '''
 
-    df = pd.read_csv(GEO)
-    df = df.drop(['Match Score'],axis=1)
+    df1 = pd.read_csv(GEO1)
+    df2 = pd.read_csv(GEO2)
+    df3 = pd.read_csv(GEO3)
+
+    df1 = df1.rename(columns={'CSDS_Org_ID':'ID'},index=str)
+
+    df = pd.concat([df1,df2,df3])
+    df = df.drop(['Match Score','ID'],axis=1)
+    df = df.rename(columns={'Zip':'ZipCode'},index=str)
+
+    df = df.drop_duplicates(subset=['Address','City','State'],keep='first')
 
     return df
 
 
 def read_dollars_divided():
     '''
-    Reads the dollars_divided file, converting the Zip field to a string.
+    Reads the dollars_divided file, converting the Zip fields to string.
     Returns a dataframe.
     '''
 
-    df = pd.read_csv(DOLLARS_DIVIDED,converters={'ZipCode_SVC':str})
+    df = pd.read_csv(DOLLARS_DIVIDED,converters={'ZipCode':str,'ZipCode_SVC':str})
+
+    cols = ['Address','City','State','ZipCode','Longitude','Latitude']
+    dicto = dict([((x, x + '_HQ')) for x in cols])
+    df = df.rename(columns=dicto,index=str)
 
     new_names = [re.sub('_SVC$','',x) for x in df.columns]
     cn = dict(zip(df.columns,new_names))
@@ -44,12 +59,14 @@ def merger(dollars_divided,geo):
     matching columns. Drops unwanted columns. Returns a dataframe.
     '''
 
-    df = u.merge_coalesce(dollars_divided,geo,'CSDS_Org_ID','_R','left')
+    df = u.merge_coalesce(dollars_divided,geo,'CSDS_Org_ID','_HQ','left')
 
     df = df.drop(['ClusterID','VendorName_LINK1','VendorName_LINK2','Name',
                   'CSDS_Vendor_ID_LINK2'],axis=1)
 
-    return df
+    subset = ['CSDS_Vendor_ID','Address','City','State','ZipCode']
+
+    return df.drop_duplicates(subset=subset).reset_index(drop=True)
 
 
 def separate_satellites(merged):
