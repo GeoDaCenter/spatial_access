b9bd41768c4bdfdade4eb592c41e8209c5b4d5f0
lnoel <lnoel@uchicago.edu>
2018-11-05 11:02:58 -0600

    Updated ConfigInterface

--- spatial_access/ConfigInterface.py
+++ spatial_access/ConfigInterface.py
@@ -16,48 +16,26 @@ class ConfigInterface():
         self.logger = logger
         self.ONE_HOUR = 3600 # seconds
         self.ONE_KM = 1000 # meters
-        self.default_edge_cost = None
-        self.load_from_file()
+        self.HUMAN_WALK_SPEED = 5
+        self.WALK_NODE_PENALTY = 0
 
+        self.DEFAULT_DRIVE_SPEED = 40
+        self.DRIVE_NODE_PENALTY = 0
 
-    def load_from_file(self):
-        '''
-        Load model parameters from json.
-        '''
+        self.BIKE_SPEED = 15.5
+        self.BIKE_NODE_PENALTY = 0
 
-        try:
-            with open(ConfigInterface.filename) as json_data:
+        self.WALK_CONSTANT = (
+            self.HUMAN_WALK_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
+        self.BIKE_CONSTANT = (
+            self.BIKE_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
+        self.DRIVE_CONSTANT = (
+            self.DEFAULT_DRIVE_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
+        if self.network_type == 'walk':
+            self.default_edge_cost = self.WALK_CONSTANT
+        elif self.network_type == 'drive':
+            self.default_edge_cost = self.DRIVE_CONSTANT
+        elif self.network_type == 'bike':
+            self.default_edge_cost = self.BIKE_CONSTANT
 
-                params = json.load(json_data)
-                self.HUMAN_WALK_SPEED = params['walk']['default_speed']
-
-                self.WALK_NODE_PENALTY = params['walk']['node_penalty']
-
-                self.DEFAULT_DRIVE_SPEED = params['drive']['default_speed']
-                self.DRIVE_NODE_PENALTY = params['drive']['node_penalty']
-
-                self.BIKE_SPEED = params['bike']['default_speed']
-                self.BIKE_NODE_PENALTY = params['bike']['node_penalty']
-
-                self.WALK_CONSTANT = (
-                    self.HUMAN_WALK_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
-                self.BIKE_CONSTANT = (
-                    self.BIKE_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
-                self.DRIVE_CONSTANT = (
-                    self.DEFAULT_DRIVE_SPEED / self.ONE_HOUR) * self.ONE_KM # meters/ second
-            if self.network_type == 'walk':
-                self.default_edge_cost = self.WALK_CONSTANT
-            elif self.network_type == 'drive':
-                self.default_edge_cost = self.DRIVE_CONSTANT
-            elif self.network_type == 'bike':
-                self.default_edge_cost = self.BIKE_CONSTANT
-            else:
-                self.logger.error("Invalid network type")
-                raise EnvironmentError("Invalid network type")
-            if self.logger:
-                self.logger.debug('Set default_edge_cost: {}'.format(self.default_edge_cost))
-        except FileNotFoundError as error:
-            if self.logger:
-                self.logger.error(error)
-            raise EnvironmentError(
-                "Config file: {} could not be found".format(ConfigInterface.filename))
+    
