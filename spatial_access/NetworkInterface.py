cb7369e71beadf6e0d2e93eaf59c77f2bca5bce5
lnoel <lnoel@uchicago.edu>
2019-03-12 14:10:05 -0500

    working on network interface

--- spatial_access/NetworkInterface.py
+++ spatial_access/NetworkInterface.py
@@ -5,6 +5,7 @@
 # pylint: skip-file
 
 import os
+import time
 import pandas as pd
 from osmnet.load import network_from_bbox
 from geopy import distance
@@ -34,6 +35,9 @@ class NetworkInterface:
         assert isinstance(network_type, str)
         self.cache_filename = 'data/osm_query_cache'
         self._try_create_cache()
+        self._second_visit = None
+        self._connected_components = None
+        self._transpose_graph = None
 
     def clear_cache(self):
         """
@@ -164,6 +168,7 @@ class NetworkInterface:
                 self.logger.info('Read network from %s', filename)
         else:
             self._request_network()
+        self._remove_disconnected_components()
 
     def _request_network(self):
         """
@@ -214,3 +219,112 @@ class NetworkInterface:
         """
         assert self.edges is not None
         return len(self.edges)
+
+    def _explore_graph_components(self, v):
+        """
+        Recursively explore graph components.
+        """
+        self._second_visit[v] = True
+
+        # add this node to the most recent component
+        self._connected_components[-1].append(v)
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self._transpose_graph[v]:
+            if not self._second_visit[i]:
+                self._explore_graph_components(i)
+
+    def _get_adjacent_vertices(self, v):
+        """
+        Return a list of vertices with edges from v.
+        """
+        return list(self.edges.iloc[self.edges.index.get_level_values(0) == v].to)
+
+    def _get_edges_as_list(self):
+        """
+        Return a list of all edges as tuples (from, to).
+        """
+        return list(zip(self.edges['from'], self.edges['to']))
+
+    def _get_vertices_as_list(self):
+        """
+        Return a list of all node ids.
+        """
+        return list(self.nodes['id'])
+
+    def _build_transpose_graph(self):
+        """
+        Build a transpose graph.
+        """
+        vertices = self._get_vertices_as_list()
+        graph = {vertex: [] for vertex in vertices}
+
+        # Recur for all the vertices adjacent to this vertex
+        for u, v in self._get_edges_as_list():
+            graph[v].append(u)
+
+        return graph
+
+    def _remove_disconnected_vertices(self, nodes_to_remove):
+        """
+        Given a set nodes_to_remove, remove them from self.nodes
+        and self.edges.
+        """
+        self.nodes = self.nodes[~self.nodes['id'].isin(nodes_to_remove)]
+        self.edges = self.edges[~self.edges['from'].isin(nodes_to_remove)]
+        self.edges = self.edges[~self.edges['to'].isin(nodes_to_remove)]
+
+    def _remove_disconnected_components(self):
+        """
+        Remove all nodes and edges that are not
+        a part of the largest strongly connected component.
+        """
+        len_edges_before = len(self.edges)
+        len_nodes_before = len(self.nodes)
+        start_time = time.time()
+        stack = []
+
+        first_visit = {vertex: False for vertex in self._get_vertices_as_list()}
+
+        nodes_to_visit = self._get_vertices_as_list()
+
+        # Determine order of nodes to visit
+        while len(nodes_to_visit) > 0:
+            v = nodes_to_visit.pop()
+            if not first_visit[v]:
+                for u in self._get_adjacent_vertices(v):
+                    if not first_visit[u]:
+                        nodes_to_visit.append(u)
+                stack.append(v)
+                first_visit[v] = True
+
+        # Create a transpose graph
+        self._transpose_graph = self._build_transpose_graph()
+
+        self._second_visit = {vertex: False for vertex in self._get_vertices_as_list()}
+        self._connected_components = [[]]
+
+        # Now process all vertices in order defined by Stack
+        while stack:
+            v = stack.pop()
+            if not self._second_visit[v]:
+                self._explore_graph_components(v)
+                self._connected_components.append([])
+
+        self._connected_components.sort(key=len, reverse=True)
+
+        assert len(self._connected_components) > 0, 'Did not find a single connected component. Stopping executing.'
+        main_component = self._connected_components[0]
+        nodes_to_remove = set(self._get_vertices_as_list()) - set(main_component)
+
+        self._remove_disconnected_vertices(nodes_to_remove)
+
+        if self.logger:
+            edges_diff = len_edges_before - len(self.edges)
+            nodes_diff = len_nodes_before - len(self.nodes)
+            time_diff = time.time() - start_time
+            self.logger.info("Removed {} edges and {} nodes which were disconnected components in {:,.2f} seconds".format(edges_diff, nodes_diff, time_diff))
+        self._connected_components = None
+        self._second_visit = None
+        self._transpose_graph = None
+
